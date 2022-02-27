aaeb782284db83238e4e4900452b5002f2c9fdbd
Ghozti <62896836+Ghozti@users.noreply.github.com>
2022-02-26 22:08:51 -0500

    Initial commit

---
+++ desktop/src/pibot/lib/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pibot.lib.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pibot.lib.DriverStation;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DriverStation(), config);
+	}
+}
