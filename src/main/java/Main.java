3f02014788a87b9c30442946a972cdaed039ae81
Ghozti <62896836+ghozti@users.noreply.github.com>
2022-03-01 21:39:23 -0500

    - worked on Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
 import com.pi4j.Pi4J;
 import com.pi4j.context.Context;
+import com.pi4j.util.Console;
 
 public class Main {
     public static void main(String[] args) {
 
         Context pi4j = Pi4J.newAutoContext();
+
+        
     }
 }
