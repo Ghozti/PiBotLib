e4cd8835a050227aa4eba26f0f4bcfa694fbdcd4
Ghozti <62896836+ghozti@users.noreply.github.com>
2022-03-04 22:17:27 -0500

    - began building the lib

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
 import com.pi4j.io.gpio.*;
+import com.pi4j.wiringpi.Gpio;
+import com.pi4j.wiringpi.SoftPwm;
 
 public class Main {
     public static void main(String[] args) {
 
         GpioController controller = GpioFactory.getInstance();
         GpioPinDigitalOutput pin = controller.provisionDigitalOutputPin(RaspiPin.GPIO_01,"pin1", PinState.HIGH);
+        Gpio.pwmWrite(pin.getPin().getAddress(), 100);
     }
 }
