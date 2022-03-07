4bcff66765583eb201f70aa4baa4b774e183569b
Ghozti <62896836+ghozti@users.noreply.github.com>
2022-03-06 22:05:08 -0500

    - did something?

--- src/main/java/GpioTest.java
+++ src/main/java/GpioTest.java
@@ -1,2 +1,12 @@
+import com.pi4j.io.gpio.*;
+import com.pi4j.wiringpi.Gpio;
+
 public class GpioTest {
+
+    public static void main(String[] args) {
+        GpioController controller = GpioFactory.getInstance();
+        GpioPinDigitalOutput pin = controller.provisionDigitalOutputPin(RaspiPin.GPIO_01,"pin1", PinState.HIGH);
+        pin.setMode(PinMode.PWM_OUTPUT);
+        Gpio.pwmWrite(pin.getPin().getAddress(), 100);
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,5 @@ import com.pi4j.wiringpi.SoftPwm;
 public class Main {
     public static void main(String[] args) {
 
-        GpioController controller = GpioFactory.getInstance();
-        GpioPinDigitalOutput pin = controller.provisionDigitalOutputPin(RaspiPin.GPIO_01,"pin1", PinState.HIGH);
-        Gpio.pwmWrite(pin.getPin().getAddress(), 100);
     }
 }
