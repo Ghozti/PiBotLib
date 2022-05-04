5e19124b62d7ad83e545e06d8f13e4a4ce2bcdf5
Ghozti <62896836+ghozti@users.noreply.github.com>
2022-05-03 21:55:45 -0400

    - updated project

--- core/src/pibot/lib/library/Robot.java
+++ core/src/pibot/lib/library/Robot.java
@@ -5,6 +5,9 @@ import com.pi4j.context.Context;
 import com.pi4j.io.gpio.digital.DigitalOutput;
 import com.pi4j.io.gpio.digital.DigitalOutputConfigBuilder;
 import com.pi4j.io.gpio.digital.DigitalState;
+import com.pi4j.io.pwm.Pwm;
+import com.pi4j.io.pwm.PwmConfig;
+import com.pi4j.io.pwm.PwmType;
 import com.pi4j.library.pigpio.PiGpio;
 import pibot.lib.utils.DriverStationState;
 
@@ -13,31 +16,44 @@ import java.util.function.Function;
 public class Robot {
 
     Context context;
-    DigitalOutputConfigBuilder pinConfig;
-    DigitalOutput pin;
+    //DigitalOutputConfigBuilder pinConfig;
+    //DigitalOutput pin;
+    Pwm pwm;
 
     public Robot(){
         context = Pi4J.newAutoContext();
-        pinConfig = DigitalOutput.newConfigBuilder(context)
-                .id("led")
-                .name("LED Flasher")
-                .address(4)
-                .shutdown(DigitalState.LOW)
-                .initial(DigitalState.LOW)
-                .provider("pigpio-digital-output");
-        pin = context.create(pinConfig);
+        //pinConfig = DigitalOutput.newConfigBuilder(context)
+        //        .id("led")
+        //        .name("LED Flasher")
+        //        .address(4)
+        //        .shutdown(DigitalState.LOW)
+        //        .initial(DigitalState.LOW)
+        //        .provider("pigpio-digital-output");
+        //pin = context.create(pinConfig);
+        pwm = context.create(buildPwmConfig(context,16));
     }
 
     public void runRobot(){
-        if (DriverStationState.getState().equals("Enabled")){
-            pin.initialize(context);
-            pin.high();
-        }
-        if (DriverStationState.getState().equals("Disabled")){
-            pin.initialize(context);
-            pin.low();
-        }else {
-            context.shutdown();
-        }
+        //if (DriverStationState.getState().equals("Enabled")){
+        //    pin.high();
+        //}
+        //if (DriverStationState.getState().equals("Disabled")){
+        //    pin.low();
+        //}else {
+        //    context.shutdown();
+        //}
+        pwm.on(50,1);
+    }
+
+    protected static PwmConfig buildPwmConfig(Context pi4j, int address) {
+        return Pwm.newConfigBuilder(pi4j)
+                .id("BCM" + address)
+                .name("Buzzer")
+                .address(address)
+                .pwmType(PwmType.HARDWARE)
+                .provider("pigpio-pwm")
+                .initial(0)
+                .shutdown(0)
+                .build();
     }
 }
